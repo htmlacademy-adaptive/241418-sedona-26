60546577e5cacb4303dd1042d25a8ed1262adc9f
mikhail radostev <radostev1@mail.ru>
2022-08-22 11:18:30 +0200

    меняет кнопку меню

---
+++ source/js/script.js
@@ -0,0 +1,18 @@
+let navMain = document.querySelector('.main-nav');
+let headerToggle = document.querySelector('.main-header__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+headerToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    headerToggle.classList.remove('main-header__toggle--closed');
+    headerToggle.classList.add('main-header__toggle--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    headerToggle.classList.add('main-header__toggle--closed');
+    headerToggle.classList.remove('main-header__toggle--opened');
+  }
+});
